66555b676a60fbde8eed8e9053d39e0c52a389b1
PeachDBC <peach@mondaystudios.com>
2020-01-14 20:29:15 -0500

    Initial Upload

---
+++ Space Shooter Professional/Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //public or private reference
+    //data type(int, float, bool, string)
+    //Start is called before the first frame update
+    //every variable has a name
+    //optional value assigned
+    [SerializeField]
+    private float _speed = 3.5f;
+
+    //start is called before the first frame update
+    void Start()
+    {
+        //take the current position = new position(0,0,0)
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //new Vector3(-5,0,0) * 5 * real time
+        transform.Translate(Vector3.right * _speed * Time.deltaTime );
+        
+    }
+}
