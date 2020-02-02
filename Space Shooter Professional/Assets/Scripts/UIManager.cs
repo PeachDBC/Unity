7c1a610349f3c9c383282a164e5c7aea3aac075d
Donna Carey <donna_b_carey@mdd.uscourts.gov>
2020-02-02 03:21:13 -0500

    Day 22

--- Space Shooter Professional/Assets/Scripts/UIManager.cs
+++ Space Shooter Professional/Assets/Scripts/UIManager.cs
@@ -12,14 +12,22 @@ public class UIManager : MonoBehaviour
     private Image _LivesImg;
     [SerializeField]
     private Sprite[] _liveSprites;
+    [SerializeField]
+    private Text _gameOverText;
+
+
     // Start is called before the first frame update
 
     void Start()
     {
         //assign text component to the handle
         _scoreText.text = "Score: " + 0;
+        _gameOverText.gameObject.SetActive(false);
     }
-
+    void Update()
+    {
+        
+    }
 
     public void UpdateScore(int playerScore)
     {
@@ -28,6 +36,21 @@ public class UIManager : MonoBehaviour
     public void UpdateLives(int currentLives)
     {
         _LivesImg.sprite = _liveSprites[currentLives];
+        if (currentLives == 0)
+        {
+            StartCoroutine(FlashTextCoroutine());
+           
+        }
 
+    }
+    IEnumerator FlashTextCoroutine()
+    {
+        while (true)
+        {
+            _gameOverText.gameObject.SetActive(true);
+            yield return new WaitForSeconds(1.0f);
+            _gameOverText.gameObject.SetActive(false);
+            yield return new WaitForSeconds(1.0f);
+        }
     }
 }
